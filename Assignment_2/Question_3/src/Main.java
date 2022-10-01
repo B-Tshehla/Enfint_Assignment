9e7947695cdc23a4f9477d284d8d84cf61221dac
Boitumelo Tshehla <Boitumelotshehla@gmail.com>
2022-10-01 17:00:21 +0200

    add comments and updated README

--- Assignment_2/Question_3/src/Main.java
+++ Assignment_2/Question_3/src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Declaration
         Scanner intInput = new Scanner(System.in);
         Integer[] numSet1 = {1,2,3,4,5};
         Integer[] numSet2 = {6,7,8,9};
@@ -13,21 +13,22 @@ public class Main {
         ArrayList<Integer> firstCollection = new ArrayList<>(Arrays.asList(numSet1));
         ArrayList<Integer> secondCollection = new ArrayList<>(Arrays.asList(numSet2));
         int value;
-
+        //Print first collection initial state
         System.out.print("First collection: ");
         for (Integer integer : firstCollection) {
             System.out.print(integer + ",");
-
         }
-
+        //Print second collection state
         System.out.print("\nSecond collection: ");
         for (Integer integer : secondCollection) {
             System.out.print(integer + ",");
         }
 
+        //User input for element index
         System.out.print("\nElement value not its index!: ");
         value = intInput.nextInt();
 
+        //Append second collection at the given index
         for (Integer integer : firstCollection) {
             if (integer == value) {
                 linkNum.add(integer);
@@ -36,9 +37,8 @@ public class Main {
                 linkNum.add(integer);
             }
         }
-
+        //Print Results
         System.out.print("Result: ");
-
         for(Integer integer : linkNum){
             System.out.print(integer+",");
         }
---
+++ Assignment_2/README.md
@@ -0,0 +1,4 @@
+# Collections
+* `Question_1` This program finds the smallest key in the Map, prints the key/value pair to the console, and removes that data from the collection.
+* `Question_2` The replace old elements with the new element in the collection (ArrayList).
+* `Question_3` The Program receives two collections of unique numbers and the value of the element as input. It is necessary to insert the second collection into the first collection starting from this element.
